ae842168d1335095bd1de4228cbd61e8344b4c7b
Michael Higginbottom <mih39@aber.ac.uk>
2015-01-21 11:29:03 +0000

    Added HTML for index.php (Requires Images). Master CSS included

---
+++ Website/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+	<!-- HEAD -->
+	<head>
+		<title>Reserve Plant Species Recording</title>
+		<meta charset="UTF-8" />
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
+	</head>
+	<!-- END HEAD -->
+
+	<!-- BODY -->
+	<body>
+
+		<!-- CONTAINER -->
+		<div class="container">
+
+		<div class="row">
+			<div class="col-12"></div>
+		</div>
+			<!-- LOGIN -->
+			<div class="row">
+				<div class="col-4"></div>
+				<div class="col-3">
+					<!-- LOGIN TABLE -->
+					<table width="4">
+						<tr>
+							<td colspan="3">LOG IN</td>
+						</tr>
+						<tr>
+							<td>Username</td>
+							<td>:</td>
+							<td><input name="myusername" type="text" id="myusername" /></td>
+						</tr>
+						<tr>
+							<td>Password</td>
+							<td>:</td>
+							<td><input name="mypassword" type="password" id="mypassword" /></td>
+						</tr>
+						<tr>
+							<td>&nbsp;</td>
+							<td>&nbsp;</td>
+							<td><input name="submit" type="submit" value="Login" /></td>
+						</tr>
+					</table>
+					<!-- END LOGIN TABLE -->
+				</div>
+				<div class="col-1">
+					<!-- IMAGE -->
+				</div>
+				<div class="col-4"></div>
+			</div>
+			<!-- END LOGIN -->
+
+		</div>
+		<!-- END CONTAINER -->
+
+	</body>
+	<!-- END BODY -->
+
+</html>
