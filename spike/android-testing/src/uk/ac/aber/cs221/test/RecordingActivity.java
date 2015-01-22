e453b58cd2515c14f1fde3e7467ddf0d826fbb43
Will <was4@aber.ac.uk>
2015-01-22 13:16:13 +0000

    set up base work for recording activity list impl

--- spike/android-testing/src/uk/ac/aber/cs221/test/RecordingActivity.java
+++ spike/android-testing/src/uk/ac/aber/cs221/test/RecordingActivity.java
@@ -1,17 +1,31 @@
 package uk.ac.aber.cs221.test;
 
+import uk.ac.aber.cs221.util.Util;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import uk.ac.aber.cs221.util.Util;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ListView;
 
 public class RecordingActivity extends Activity {
-
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		Util.setupActionBar(this);
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_recording);
-	}
+   
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      Util.setupActionBar(this);
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_recording);
+      setupList();
+   }
+   
+   private void setupList() {
+      ListView list = (ListView) findViewById(R.id.ra_speciesList);
+      list.setOnClickListener(new OnClickListener() {
+         @Override
+         public void onClick(View v) {
+            
+         }
+      });
+   }
 }
