74b2dbe805c72c9f92d99f0631298d0b1af5bd28
Michael Higginbottom <mih39@aber.ac.uk>
2015-01-22 17:01:07 +0000

    added nav bar CSS

--- Website/navbar.php
+++ Website/navbar.php
@@ -2,20 +2,23 @@
 //nav bar
 // Author: Niall
 -->
-				<div class="row">
-					<div class="col-4">
+				<link rel="stylesheet" type="text/css" href="css/master.css" />
+				<link rel="stylesheet" type="text/css" href="css/navbar.css" />
+				<div class="row list">
+					<div class="col-3 listelement">
 						<a href="reservelist.php">Reserve List</a>
 					</div>	
-					<div class="col-4">
+					<div class="col-3 listelement">
 						<!-- TODO: ADD THE USERNAME AFTER THIS -->
 						<a href="reservelist.php">User Page</a>
 					</div>
-					<div class="col-4">
+					<div class="col-3 listelement">
 						<a href="addreserve.php">Add Reserve</a>
 					</div>
+					<div class="col-3 listelement">
+						<a href="php/logout.php">Logout</a>
+					</div>
 				</div>
-				
-				<a href="php/logout.php">Logout</a>
 				
 				<a href="specieslist.php">Species</a>
 
