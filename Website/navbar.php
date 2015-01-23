cf8ec2873eeda9dfab36549b537d064fe2476cb0
Kaloyan Cholakov <kaloyanch@gmail.com>
2015-01-23 11:29:10 +0000

    Some work on Navbar

--- Website/navbar.php
+++ Website/navbar.php
@@ -5,23 +5,16 @@
 
 <link rel="stylesheet" type="text/css" href="css/master.css" />
 <link rel="stylesheet" type="text/css" href="css/navbar.css" />
-<div class="row list">
-<div class="col-2 logo">
-<img src="logoNav.png">
-</div>
-<div class="col-2 listelement">
-<a href="reservelist.php">RESERVE LIST</a>
-</div>
-<div class="col-2 listelement">
-<!-- TODO: ADD THE USERNAME AFTER THIS -->
-<a href="reservelist.php?name=<?php echo $_SESSION['user']; ?>">USER PAGE</a>
-</div>
-<div class="col-2 listelement">
-<a href="addreserve.php">ADD RESERVE</a>
-</div>
-<div class="col-2 listelement">
-<a href="php/logout.php">LOGOUT</a>
-</div>
-</div>
-
-  
+	<div class="row list">
+		<div class="col-12 logo">
+			<!-- CSS IN NAV BACKGROUND -->
+			<ul>
+				<li class="col-2 logo">
+				<li class="listelement"><a href="reservelist.php">RESERVE LIST</a></li>
+				<li class="listelement"><a href="addreserve.php">ADD RESERVE</a></li>
+				<li class="listelement"><a href="php/logout.php">LOGOUT</a><li>
+				<li class="listelement"><a href="reservelist.php?name=<?php echo $_SESSION['user']; ?>">USER PAGE</a></li>
+			</ul>
+		</div>
+		
+	</div>
