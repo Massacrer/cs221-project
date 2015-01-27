81b8e03080e0966ccbbd474bec76f59b3b3cfeb0
luke93 <luh17@aber.ac.uk>
2015-01-27 09:49:54 +0000

    added to record species

--- spike/android-testing/src/uk/ac/aber/cs221/test/PhotoPickerActivity.java
+++ spike/android-testing/src/uk/ac/aber/cs221/test/PhotoPickerActivity.java
@@ -36,14 +36,14 @@ import android.widget.Toast;
  * @author was4
  */
 public class PhotoPickerActivity extends Activity {
-   
+
    private File currentImage;
-   
+
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_photo_picker);
-      
+
       findViewById(R.id.pp_CameraButton).setOnClickListener(
             new OnClickListener() {
                public void onClick(View v) {
@@ -57,7 +57,7 @@ public class PhotoPickerActivity extends Activity {
                }
             });
    }
-   
+
    /**
     * Opens the device camera app to take a picture, and informs the device
     * media storage of the new image so that it can be added to e.g. the Gallery
@@ -66,10 +66,10 @@ public class PhotoPickerActivity extends Activity {
    private void takePicture() {
       String now = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
       // vFile picture = new File(this.getFilesDir(), "img_" + now);
-      
+
       Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
       Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-      
+
       try {
          File extDir = Environment
                .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
@@ -83,32 +83,30 @@ public class PhotoPickerActivity extends Activity {
           */
          cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,
                Uri.fromFile(currentImage));
-         
+
          Uri uri = new Uri.Builder().path(currentImage.getPath()).build();
          // DEBUG
          ((TextView) findViewById(R.id.pp_CameraButton))
                .setText(uri.toString());
          Log.i("CS221_test", uri.toString());
-         
+
          mediaScanIntent.setData(uri);
          this.sendBroadcast(mediaScanIntent);
-         
-      }
-      catch (IOException e) {
+
+      } catch (IOException e) {
          Toast.makeText(this, "Could not save image file", Toast.LENGTH_LONG)
                .show();
          return;
       }
-      
+
       if (cameraIntent.resolveActivity(getPackageManager()) != null) {
          startActivityForResult(cameraIntent, 1);
-      }
-      else {
+      } else {
          Toast.makeText(this, "No camera app available", Toast.LENGTH_LONG)
                .show();
       }
    }
-   
+
    /**
     * Opens the device Media Picker activity, allowing the user to select an
     * image, whos filename is then returned to onActivityResult
@@ -119,40 +117,43 @@ public class PhotoPickerActivity extends Activity {
       intent.setType("image/*");
       startActivityForResult(Intent.createChooser(intent, "Select File"), 2);
    }
-   
+
    @Override
    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
       super.onActivityResult(requestCode, resultCode, data);
       if (resultCode == RESULT_OK) {
          switch (requestCode) {
          // 1 -> camera result
-            case 1: {
-               Intent mediaScanIntent = new Intent(
-                     Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-               Uri contentUri = Uri.fromFile(currentImage);
-               mediaScanIntent.setData(contentUri);
-               this.sendBroadcast(mediaScanIntent);
-               
-               displayBitmap();
-               break;
-            }
-            // 2 -> gallery result
-            case 2:
-               Uri image = data.getData();
-               String[] path = { MediaStore.Images.Media.DATA };
-               Cursor cursor = getContentResolver().query(image, path, null,
-                     null, null);
-               cursor.moveToFirst();
-               
-               currentImage = new File(cursor.getString(cursor
-                     .getColumnIndex(path[0])));
-               cursor.close();
-               displayBitmap();
-               break;
+         case 1: {
+            Intent mediaScanIntent = new Intent(
+                  Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+            Uri contentUri = Uri.fromFile(currentImage);
+            mediaScanIntent.setData(contentUri);
+            this.sendBroadcast(mediaScanIntent);
+
+            displayBitmap();
+            showPhotoChoiceAlert();
+            break;
+         }
+         // 2 -> gallery result
+         case 2:
+            Uri image = data.getData();
+            String[] path = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(image, path, null, null,
+                  null);
+            cursor.moveToFirst();
+
+            currentImage = new File(cursor.getString(cursor
+                  .getColumnIndex(path[0])));
+            cursor.close();
+            displayBitmap();
+            showPhotoChoiceAlert();
+
+            break;
          }
       }
    }
-   
+
    /**
     * Displays the bitmap image stored in currentImage in the main view,
     * downscaling it as necessary to fit on the screen and minimise memory use
@@ -160,22 +161,22 @@ public class PhotoPickerActivity extends Activity {
    private void displayBitmap() {
       ImageView imageView = (ImageView) findViewById(R.id.imageView1);
       imageView.setVisibility(View.VISIBLE);
-      
+
       BitmapFactory.Options bmOptions = new BitmapFactory.Options();
       bmOptions.inJustDecodeBounds = true;
       BitmapFactory.decodeFile(currentImage.getPath(), bmOptions);
-      
+
       /*
        * int scaleFactor = calculateInSampleSize(bmOptions,
        * imageView.getWidth(), imageView.getHeight());
        */
-      
+
       /*
        * int photoW = bmOptions.outWidth; int photoH = bmOptions.outHeight; int
        * scaleFactor = Math.min(photoW / imageView.getWidth(), photoH /
        * imageView.getHeight());
        */
-      
+
       float rotation = 0;
       try {
          ExifInterface eif = new ExifInterface(currentImage.getPath());
@@ -186,38 +187,75 @@ public class PhotoPickerActivity extends Activity {
             rotation = 180;
          if (orientation.equals("8"))
             rotation = 270;
+      } catch (IOException e) {
       }
-      catch (IOException e) {
-      }
-      
+
       boolean sideways = rotation == 90 || rotation == 270;
-      
+
       int vW = imageView.getWidth();
       int vH = imageView.getHeight();
-      
+
       int scaleFactor = bmOptions.inSampleSize = Math.max(bmOptions.outWidth
             / vW, bmOptions.outHeight / vH);
-      
+
       Toast.makeText(this, "scale: " + scaleFactor, Toast.LENGTH_LONG).show();
-      
+
       bmOptions.inJustDecodeBounds = false;
       // bmOptions.inSampleSize = scaleFactor;
       bmOptions.inPurgeable = true;
-      
+
       Bitmap bitMap = BitmapFactory.decodeFile(currentImage.getPath(),
             bmOptions);
-      
+
       Matrix matrix = new Matrix();
       matrix.preRotate(rotation);
-      
+
       bitMap = Bitmap.createBitmap(bitMap, 0, 0, bitMap.getWidth(),
             bitMap.getHeight(), matrix, true);
-      
+
       bitMap = Bitmap.createScaledBitmap(bitMap, bitMap.getWidth()
             / scaleFactor, bitMap.getHeight() / scaleFactor, false);
-      
+
       imageView.setImageBitmap(bitMap);
-      
+
    }
-   
+
+   public void showPhotoChoiceAlert() {
+
+      AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+      alertDialog.setTitle("Photo choice");
+      alertDialog.setMessage("Is this the photo you wish to use?");
+
+      alertDialog.setPositiveButton("Yes",
+            new DialogInterface.OnClickListener() {
+
+               @Override
+               public void onClick(DialogInterface dialog, int which) {
+
+                  Intent data = new Intent();
+                  data.putExtra("fileName", currentImage.getAbsolutePath());
+                  data.putExtra("picture", PhotoPickerActivity.this.getIntent()
+                        .getStringExtra("picture"));
+                  // Activity finished ok, return the data
+                  setResult(RESULT_OK, data);
+                  finish();
+
+                  // PhotoPickerActivity.this.finish();
+
+               }
+            });
+
+      alertDialog.setNegativeButton("No",
+            new DialogInterface.OnClickListener() {
+
+               @Override
+               public void onClick(DialogInterface dialog, int which) {
+                  dialog.cancel();
+
+               }
+            });
+
+      alertDialog.show();
+
+   }
 }
