f46e75108b17f92401f2e88d01b9074c8b3f95ff
Will <was4@aber.ac.uk>
2015-01-27 01:43:01 +0000

    working on converting stored objects to JSON for uploading

--- spike/android-testing/src/uk/ac/aber/cs221/upload/StorageInterface.java
+++ spike/android-testing/src/uk/ac/aber/cs221/upload/StorageInterface.java
@@ -1,5 +1,51 @@
 package uk.ac.aber.cs221.upload;
 
+import java.io.OutputStreamWriter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.util.JsonWriter;
+import uk.ac.aber.cs221.storage.Recording;
+import uk.ac.aber.cs221.storage.RecordingStorage;
+import uk.ac.aber.cs221.storage.Storage;
+import static org.json.JSONObject.quote;
+
 public class StorageInterface {
+   public JSONObject getRecording(Context context, long id) {
+      Recording recording = RecordingStorage.getInstance(context).get(id);
+      JSONObject values = new JSONObject();
+      try {
+         values.put("name", quote(recording.name));
+         values.put("description", quote(recording.description));
+         values.put(
+               "latitude",
+               recording.loc == null ? "" : JSONObject
+                     .numberToString(recording.loc.getLatitude()));
+         values.put(
+               "longitude",
+               recording.loc == null ? "" : JSONObject
+                     .numberToString(recording.loc.getLongitude()));
+         values.put(
+               "date",
+               recording.date == null ? null : Storage
+                     .dateTimeString(recording.date));
+         
+         values.put("user_name", quote(recording.userName));
+         values.put("user_number", quote(recording.userNumber));
+         values.put("user_email", quote(recording.userEmail));
+         JSONArray speciesArray = new JSONArray();
+         values.put("species", speciesArray);
+         
+         // not needed
+         // JsonWriter writer = new JsonWriter(new
+         // OutputStreamWriter(System.out));
+      }
+      catch (JSONException e) {
+      }
+      return null;
+   }
    
 }
